7c80aaa95da2dc96aaee31644a015fd55f1a1426
Dinewo <dididlalisa@gmail.com>
2024-07-12 15:13:44 +0200

    Added HTML, CSS and JavaScript for implementation of a simple interactive quiz

---
+++ quiz.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function checkAnswer() {
+        
+        let userAnswer = document.querySelector('input[name="quiz"]:checked');
+        
+    
+        userAnswer = userAnswer.value;
+        
+    
+        let correctAnswer = "4"; 
+        
+        
+        let feedback = document.getElementById('feedback');
+
+
+        if (userAnswer === correctAnswer) {
+            feedback.textContent = "Correct! Well done.";
+        } else {
+            feedback.textContent = "That's incorrect. Try again!";
+        }
+    }
+
+    document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
